8a48a25c5cfc5754c5a8524f40f05e86ff873220
Rene Dellefont <renedellefont@gmail.com>
2019-05-18 22:10:31 -0400

    Add lambda function

---
+++ functions/contact.js
@@ -0,0 +1,10 @@
+exports.handler = async event => {
+  console.log(event);
+
+  try {
+    const subject = event.queryStringParameters.name || "World";
+    return { statusCode: 200, body: `Hello ${subject}` };
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
+  }
+};
